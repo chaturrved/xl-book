55af6d7cdd434630dfdce2ceda007602c5d9ea3d
chaturrved <chaturved.edu@gmail.com>
2024-02-01 11:12:05 +0530

    formula evaluation fixed and cell dependency implemented

--- formula.js
+++ formula.js
@@ -4,11 +4,20 @@ for (let i = 0; i < rows; i++) {
     cell.addEventListener("blur", (e) => {
       let address = addressBar.value;
       let [cell, cellProps] = activeCell(address);
+      let enteredData = cell.innerText;
+
+      if (enteredData == cellProps.value) {
+        return;
+      }
+
       //Modification of data and ui
       cellProps.value = cell.innerText;
 
+      removeRelationship(cellProps, address);
+      updateChildren(address);
+      cellProps.formula = "";
       //remove formula from formular bar
-      formulaBar.value = "";
+      //formulaBar.value = "";
 
       //scroll to top
       cell.scrollTop = 0;
@@ -22,7 +31,8 @@ formulaBar.addEventListener("keydown", (e) => {
   const formula = formulaBar.value;
   if (e.key == "Enter" && formula) {
     //remove relationship
-    let [cell, cellProps] = activeCell(addressBar.value);
+    let [rid, cid] = decodeCellFromAddress(addressBar.value);
+    let cellProps = sheetDB[rid][cid];
     let parentsList = cellProps.formula.split(" ");
     let currentParentsList = formula.split(" ");
     parentsList.forEach((parent) => {
@@ -34,15 +44,19 @@ formulaBar.addEventListener("keydown", (e) => {
 
     //evaluate current formula
     let evaluatedValue = evaluateFormula(formula);
+    establishRelationship(addressBar.value, formula);
     setCellValueAndFormula(evaluatedValue, formula, addressBar.value);
+    updateChildren(addressBar.value);
   }
 });
 
 function updateChildren(parentAddress) {
-  let [parentCell, parentCellProps] = activeCell(parentAddress);
+  let [prid, pcid] = decodeCellFromAddress(parentAddress);
+  let parentCellProps = sheetDB[prid][pcid];
   let children = parentCellProps.children;
   children.forEach((child) => {
-    let [childCell, childCellProps] = activeCell(child);
+    let [crid, ccid] = decodeCellFromAddress(child);
+    let childCellProps = sheetDB[crid][ccid];
     let evaluatedValue = evaluateFormula(childCellProps.formula);
     setCellValueAndFormula(evaluatedValue, childCellProps.formula, child);
     updateChildren(child);
@@ -50,40 +64,54 @@ function updateChildren(parentAddress) {
 }
 
 function evaluateFormula(formula) {
-  let encodedFormula = formula.split(" ");
-  for (let i = 0; i < encodedFormula.length; i++) {
-    let code = encodedFormula[i].charCodeAt(0);
-    if (code >= 65 && code <= 90) {
-      let [cell, cellProps] = activeCell(encodedFormula[i]);
-      establishRelationship(encodedFormula[i]); //maintain relations to update values on change
-      encodedFormula[i] = cellProps.value;
+  let decodedFormula = formula.split(" "); // Decode formula for easy parsing
+  for (let i = 0; i < decodedFormula.length; i++) {
+    let asciiVal = decodedFormula[i].charCodeAt(0);
+    if (asciiVal >= 65 && asciiVal <= 90) {
+      // Check for a valid address to evaluate
+      let [rid, cid] = decodeCellFromAddress(decodedFormula[i]);
+      let cellProp = sheetDB[rid][cid];
+      decodedFormula[i] = cellProp.value;
     }
   }
   //console.log(sheetDB);
-  formula = encodedFormula.join(" ");
+  formula = decodedFormula.join(" ");
   return eval(formula);
 }
 
 function setCellValueAndFormula(value, formula, address) {
-  let [cell, cellProps] = activeCell(address);
+  let [rid, cid] = decodeCellFromAddress(address);
+  let cell = grid.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+  let cellProps = sheetDB[rid][cid];
   cellProps.value = value;
   cellProps.formula = formula;
   cell.innerText = value;
 }
 
-function establishRelationship(parent) {
-  let p = parent.split("");
-  let column = p[0].charCodeAt(0) - 65;
-
-  if (!sheetDB[p[1] - 1][column].children.includes(addressBar.value)) {
-    sheetDB[p[1] - 1][column].children.push(addressBar.value);
+function establishRelationship(childAddress, formula) {
+  let decodedFormula = formula.split(" ");
+  for (let i = 0; i < decodedFormula.length; i++) {
+    let asciiVal = decodedFormula[i].charCodeAt(0);
+    if (asciiVal >= 65 && asciiVal <= 90) {
+      // If valid parent -> add child(active) cell in parent
+      let parentAddress = decodedFormula[i];
+      let [rid, cid] = decodeCellFromAddress(parentAddress);
+      let parentProp = sheetDB[rid][cid];
+      parentProp.children.push(childAddress);
+    }
   }
 }
 
-function removeRelationship(parent, child) {
-  let p = parent.split("");
-  let column = p[0].charCodeAt(0) - 65;
-
-  let index = sheetDB[p[1] - 1][column].children.indexOf(child);
-  sheetDB[p[1] - 1][column].children.splice(index, 1);
+function removeRelationship(cellProp, activeAddress) {
+  let formula = cellProp.formula;
+  let decodedFormula = formula.split(" ");
+  for (let i = 0; i < decodedFormula.length; i++) {
+    let asciiVal = decodedFormula[i].charCodeAt(0);
+    if (asciiVal >= 65 && asciiVal <= 90) {
+      let [rid, cid] = decodeCellFromAddress(decodedFormula[i]);
+      let parentCellProp = sheetDB[rid][cid];
+      let removeIdx = parentCellProp.children.indexOf(activeAddress); // Get index of child from parent to remove child
+      parentCellProp.children.splice(removeIdx, 1); // Remove child in parent
+    }
+  }
 }
